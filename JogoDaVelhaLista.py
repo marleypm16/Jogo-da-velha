503e063d29d6641a21a04bca8ae5304c8ee145e8
marleypm16 <yarlempm6@gmail.com>
2022-05-30 10:28:27 -0300

    Jogo Da Velha utilizando lista

---
+++ JogoDaVelhaLista.py
@@ -0,0 +1,80 @@
+# fazer a variavel com meu jogo, vitoria, jogador, jogorodando
+
+
+jogo= ['-','-','-','-','-','-','-','-','-']
+JogoEmAndamento= True
+jogador = 'X'
+#funcao para mostrar o jogo 
+
+def MostrarJogo(jogo):
+    print(jogo[0] + "|" + jogo[1] + "|" + jogo[2])
+    print(jogo[3] + '|' + jogo[4] + "|" +jogo[5])
+    print(jogo[6] + '|' + jogo[7] + "|" +jogo[8])
+
+#Perguntar ao jogador onde ele vai colocar sua letra
+
+def ask_player(game):
+    ask= int(input(" Em qual posição voce quer inserir? Escolha de 1 a 9 :"))
+    if game[ask -1] == '-':
+        game[ask - 1] = jogador
+
+
+#Verificar vitorias
+
+def check_Horizontal(jogo):
+    vitoria= jogo[0] == jogo[1] == jogo[2] and jogo[0] !='-' or jogo[3] ==  jogo[4] == jogo[5] and jogo[3] != '-' or jogo[6] ==  jogo[7] == jogo[8] and jogo[6] != '-'
+    if vitoria:
+        MostrarJogo(jogo)
+        print('Vitória')
+        return True
+        
+
+def check_Vertical(jogo):
+    vitoria= jogo[0] == jogo[3] == jogo[6] and jogo[0]!= '-' or jogo[1] ==  jogo[4] == jogo[7] and jogo[1] !='-' or jogo[2] ==  jogo[5] == jogo[8] and jogo[2] != '-'
+    if vitoria:
+        MostrarJogo(jogo)
+        print('Vitória')
+        return True
+
+
+
+def check_Diagonal(jogo):
+    vitoria= jogo[0] == jogo[4] == jogo[8] and jogo[0] != '-' or jogo[2] ==  jogo[4] == jogo[6] and jogo[2] != '-'
+    if vitoria:
+        MostrarJogo(jogo)
+        print('Vitória')
+        return True
+
+
+
+#Empate
+
+def Empate(jogo):
+    if '-' not in jogo:
+        print("Empate")
+        MostrarJogo(jogo)
+        return True
+
+#Trocar Jogadores
+
+
+def Trocar_Jogadores():
+    global jogador
+    if jogador == 'X':
+        jogador = 'O'
+    else:
+        jogador= 'X'
+
+
+while JogoEmAndamento:
+    MostrarJogo(jogo)
+    ask_player(jogo)
+    if check_Diagonal(jogo) or check_Horizontal(jogo) or check_Vertical(jogo):
+        JogoEmAndamento= False
+
+    if Empate(jogo):
+        JogoEmAndamento = False
+   
+    Trocar_Jogadores()
+
+
